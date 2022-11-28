9086d1cf474e852bd0988abf15aa1d69ef4d06ec
kushagra sharma <kushagrasharma30072001@gmail.com>
2022-11-29 03:32:51 +0530

    added dialog box for confirmation of pause and resume

--- src/gui/vue/pages/captureControl/expCapture/organisms/captureToolHeader/PauseButton.vue
+++ src/gui/vue/pages/captureControl/expCapture/organisms/captureToolHeader/PauseButton.vue
@@ -16,17 +16,50 @@
 
 <template>
   <div>
-    <v-btn
-      :disabled="!isCapturing"
-      @click="pushPauseButton"
-      icon
-      flat
-      large
-      :title="pauseButtonTooltip"
-      :color="pauseButtonColor"
-    >
-      <v-icon>pause</v-icon>
-    </v-btn>
+    <v-layout>
+      <v-btn
+        :disabled="!isCapturing"
+        @click="ConfirmationModelVisisbility = true"
+        icon
+        flat
+        large
+        :title="pauseButtonTooltip"
+        :color="pauseButtonColor"
+      >
+        <v-icon>pause</v-icon>
+      </v-btn>
+    </v-layout>
+    <v-layout row justify-centre>
+      <v-dialog
+        v-model="ConfirmationModelVisisbility"
+        presistent
+        max-width="290"
+      >
+        <v-card>
+          <v-card-title class="error headline" style="font-weight: bold">{{
+            $store.getters.message("confirmation_dialog_box.card-title")
+          }}</v-card-title>
+          <v-card-text>{{
+            $store.getters.message("confirmation_dialog_box.card-content")
+          }}</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat @click="ConfirmationModelVisisbility = false">{{
+              $store.getters.message("confirmation_dialog_box.card-button-1")
+            }}</v-btn>
+            <v-btn
+              color="error"
+              flat
+              @click="pushPauseButton"
+              v-on:click="ConfirmationModelVisisbility = false"
+              >{{
+                $store.getters.message("confirmation_dialog_box.card-button-2")
+              }}</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
   </div>
 </template>
 
@@ -62,6 +95,11 @@ export default class PauseButton extends Vue {
     } else {
       this.$store.dispatch("captureControl/pauseCapturing");
     }
+  }
+  data() {
+    return {
+      ConfirmationModelVisisbility: false,
+    };
   }
 }
 </script>
