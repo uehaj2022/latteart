366e642d8c2e5b12c003cfe0114564c6a3fcaa86
SREELAKSHMI ANEESH <87542050+Sreelakshmi-Aneesh@users.noreply.github.com>
2023-01-10 20:43:12 +0530

    Update OperationList.vue
    Adding a search bar with clickable search icon.
    
    
    Signed-off-by: Sreelakshmi Aneesh sreelakshmianeesh05@gmail.com

--- src/gui/components/pages/operationHistory/organisms/OperationList.vue
+++ src/gui/components/pages/operationHistory/organisms/OperationList.vue
@@ -159,17 +159,7 @@
         :label="message('operation.notice')"
         v-model="noticeCheckbox"
       ></v-checkbox>
-      <template>
-        <v-text-field
-        class="search-item"
-        v-model="search"
-        :label="message('operation.query')"
-      ></v-text-field>
-    <div class="search-container">
-      <input type="text" class="search-item" v-model="search" />
-      <button class="search-button" @click="search" style="display: flex; justify-content: center; align-items: center;">Search<v-icon>search</v-icon></button>
-   </div>
-   </template>
+      
     </v-layout>
 
     <operation-context-menu
