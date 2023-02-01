c399456c1a3d0920ab82f3d8092dd342743cbcd9
SREELAKSHMI ANEESH <87542050+Sreelakshmi-Aneesh@users.noreply.github.com>
2023-02-01 19:57:06 +0530

    fix uehaj2022 / latteart#56

--- src/gui/components/pages/captureControl/expCapture/organisms/captureToolHeader/CaptureToolHeader.vue
+++ src/gui/components/pages/captureControl/expCapture/organisms/captureToolHeader/CaptureToolHeader.vue
@@ -15,6 +15,25 @@
 -->
 
 <template>
+  <v-layout
+  justify-space-between
+  align-space-between
+  row
+  @keydown="cancelKeydown"
+>
+  <div>
+    <v-layout>
+      <url-text-field />
+      <test-result-name-text-field />
+      <v-btn
+        color="primary"
+        @click="submitForm"
+      >
+        Submit
+      </v-btn>
+    </v-layout>
+  </div>
+</v-layout>
   <v-layout justify-start align-center row @keydown="cancelKeydown">
     <url-text-field />
     <test-result-name-text-field />
@@ -24,10 +43,57 @@
         <record-button />
         <pause-button />
         <clear-history-button />
+
         <load-history-button />
         <menu-button />
       </v-layout>
     </div>
+    <div>
+  <v-layout>
+    <load-history-button />
+    <menu-button />
+    <v-spacer />
+    <v-icon @click="openSettings">settings</v-icon>
+  </v-layout>
+</div>
+<v-menu
+  top
+  offset-y
+  origin="center center"
+  transition="scale-transition"
+  :close-on-content-click="false"
+>
+  <template v-slot:activator="{ on, attrs }">
+    <v-btn icon flat large v-bind="attrs" v-on="on">
+      <v-icon>menu</v-icon>
+    </v-btn>
+  </template>
+
+
+  <v-list>
+    <v-list-item>
+      <v-list-item-title>
+        <v-flex shrink pa-1 pl-3>
+          <locale-select-box />
+        </v-flex>
+      </v-list-item-title>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-title>
+        <v-flex shrink pa-1 pl-3>
+          <remote-access-field color="inherit" />
+        </v-flex>
+      </v-list-item-title>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-title>
+        <v-flex shrink pa-1 pl-3>
+          <v-switch label="Dark Mode" v-model="darkMode" />
+        </v-flex>
+      </v-list-item-title>
+    </v-list-item>
+  </v-list>
+</v-menu>
 
     <v-flex shrink pa-1 pl-3>
       <locale-select-box />
@@ -38,34 +104,13 @@
 </template>
 
 <script lang="ts">
-import RemoteAccessField from "@/components/pages/common/organisms/RemoteAccessField.vue";
-import { Component, Vue } from "vue-property-decorator";
-import ClearHistoryButton from "./ClearHistoryButton.vue";
-import GenerateTestScriptButton from "./GenerateTestScriptButton.vue";
-import LoadHistoryButton from "./LoadHistoryButton.vue";
-import LocaleSelectBox from "./LocaleSelectBox.vue";
-import PauseButton from "./PauseButton.vue";
-import RecordButton from "./RecordButton.vue";
-import TestResultNameTextField from "./TestResultNameTextField.vue";
-import URLTextField from "./URLTextField.vue";
-import MenuButton from "./MenuButton.vue";
-
-@Component({
-  components: {
-    "url-text-field": URLTextField,
-    "test-result-name-text-field": TestResultNameTextField,
-    "record-button": RecordButton,
-    "pause-button": PauseButton,
-    "clear-history-button": ClearHistoryButton,
-    "load-history-button": LoadHistoryButton,
-    "generate-test-script-button": GenerateTestScriptButton,
-
-    "locale-select-box": LocaleSelectBox,
-    "remote-access-field": RemoteAccessField,
-    "menu-button": MenuButton,
+@@ -122,9 +66,6 @@ import MenuButton from "./MenuButton.vue";
   },
 })
 export default class CaptureToolHeader extends Vue {
+submitForm: any;
+openSettings: any;
+darkMode: any;
   private cancelKeydown(event: Event) {
     event.stopPropagation();
   }
